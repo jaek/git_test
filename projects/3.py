90c779bc567889bca39d2db521282f1b168381cc
Jake <jake.w.burns@gmail.com>
2017-02-24 16:48:41 +1100

    optimised answer 3

--- projects/3.py
+++ projects/3.py
@@ -1,18 +1,18 @@
-import math
+from math import sqrt
 
 # generate a list of primes less than NUM
 NUM = 600851475143
 highest = 0
 
 def is_prime(number):
-    for i in xrange(2, int(math.sqrt(number)+1)):
+    for i in xrange(2, int(sqrt(number)+1)):
         if number % i == 0:
             return False
     return True
 
 def factors(number):
     highestPrime = 0
-    for i in xrange(1, int(math.sqrt(number)+1)):
+    for i in xrange(1, int(sqrt(number)+1)):
         if not number % i:
             if is_prime(i) or is_prime(int(number / i)):
                 highestPrime = i
